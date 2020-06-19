6d33dc5533236fcd55062c0377e381e1931367b3
Your Name <you@example.com>
2020-06-19 18:25:47 +1000

    secod add

--- src/main/java/com/qa/pages1/Login1Page.java
+++ src/main/java/com/qa/pages1/Login1Page.java
@@ -4,4 +4,8 @@ public class Login1Page {
 	public void Login1Page(){
 		System.out.println("Login1Page method");
 	}
+	
+	public void Login2Page(){
+		System.out.println("Login2Page method");
+	}
 }
---
+++ src/main/java/com/qa/pages1/SearchPAge.java
@@ -0,0 +1,5 @@
+package com.qa.pages1;
+
+public class SearchPAge {
+
+}
